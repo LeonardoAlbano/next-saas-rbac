b991a28ba824c2cf9fdd004455468616e24534cb
Leonardo Albano Mondini <leonardoalbanom@yahoo.com.br>
2025-02-11 19:16:23 -0300

    jhdsj

--- packages/auth/src/subjects/user.ts
+++ packages/auth/src/subjects/user.ts
@@ -1,3 +1,12 @@
-export type UserSubject = [
-  'create' | 'delete' | 'invite' | 'manage' | 'Project',
-]
+import { z } from 'zod'
+export const userSubject = z.tuple([
+  z.union([
+    z.literal('manage'),
+    z.literal('get'),
+    z.literal('update'),
+    z.literal('delete'),
+  ]),
+  z.literal('User'),
+])
+
+export type UserSubject = z.infer<typeof userSubject>
