092ea2d6e582049bb72ff877676345c4c444e94a
Leonardo Albano <leonardoalbanom@yahoo.com.br>
2025-02-13 14:42:35 -0300

    config errors handlers

--- apps/api/src/http/routes/auth/authenticate-with-password.ts
+++ apps/api/src/http/routes/auth/authenticate-with-password.ts
@@ -2,7 +2,8 @@ import { compare } from "bcryptjs";
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from 'zod'
-import { prisma } from "../lib/prisma";
+import { prisma } from "../../../lib/prisma";
+import { BadRequestError } from "../_errors/bad-request-error";
 
 export async function authenticateWithPassword(app: FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>().post(
@@ -15,6 +16,11 @@ export async function authenticateWithPassword(app: FastifyInstance) {
                 email: z.string().email(),
                 password: z.string(),
             }),
+            response: {
+                201: z.object({
+                    token: z.string(),
+                })
+            }
         },
     },
     async (request, reply) => {
@@ -25,13 +31,11 @@ export async function authenticateWithPassword(app: FastifyInstance) {
         })
 
         if(!userFromEmail) {
-            return reply.status(400).send({ message: 'Invalid credentials' })
+            throw new BadRequestError('Invalid credentials')
         }
 
         if ( userFromEmail.passwordHash === null ) {
-            return reply 
-             .status(400)
-             .send({ message: 'User does not have a password, use social login' })
+            throw new BadRequestError('User does not have a password, use social login')
         }
 
         const isPasswordValid = await compare(
@@ -40,7 +44,7 @@ export async function authenticateWithPassword(app: FastifyInstance) {
         )
 
         if (!isPasswordValid) {
-            return reply.status(400).send({ message: 'Invalid credentials' })
+            throw new BadRequestError('Invalid credentials')
         }
 
 
--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,15 +1,16 @@
 import type { FastifyInstance } from "fastify";
 import type { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
-import { prisma } from "../lib/prisma";
+import { prisma } from "../../../lib/prisma";
 import { hash } from "bcryptjs";
+import { BadRequestError } from "../_errors/bad-request-error";
 
 export async function createAccount(app: FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>().post(
     '/users', 
     {
         schema: {
-         tags: ['Auth'],
+         tags: ['auth'],
          summary: 'Create a new account',
             body: z.object({
                 name: z.string(),
@@ -28,9 +29,7 @@ export async function createAccount(app: FastifyInstance) {
        })
 
        if (userWithSameEmail) {
-        return reply
-            .status(400)
-            .send({ message: 'user with same e-mail already exists.' })
+        throw new BadRequestError('user with same e-mail already exists.')
        }
 
        const [, domain ] = email.split('@')
